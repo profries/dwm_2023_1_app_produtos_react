e0b0d8858c312406e02940817b98308b48f8ae16
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-05-20 10:36:12 -0300

    Criacao do CardProduto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import produtos from './data/produtos.json';
+import CardProduto from './componentes/CardProduto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="w3-container w3-teal">
+      <h1>Hello World</h1>
     </div>
-  );
+    <CardProduto 
+      produto = {produtos[0]}>
+    </CardProduto>
+    </>
+);
 }
 
 export default App;
